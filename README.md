395f37a95b87dee3ef732ade0143a34f38df4348
LiShuoCommunity <1350159649@qq.com>
2020-03-02 21:14:10 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+##李硕社区
+
+##资料
+[Test](www.baidu.com)
+www.qq.com
+www.cc.com
+sss
---
+++ src/main/java/com/test/springcommunity/SpringCommunityApplication.java
@@ -0,0 +1,15 @@
+package com.test.springcommunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCommunityApplication.class, args);
+    }
+
+
+
+}
